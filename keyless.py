e8e81884f6f0709f0a77a85f772b35b88e1d88f4
Ankush Jain <33591091+ankush17100@users.noreply.github.com>
2019-08-26 11:33:03 +0530

    Update keyless.py
    Commented out the matrix implementation, did the encryption on string instead.

--- keyless.py
+++ keyless.py
@@ -1,6 +1,20 @@
 print('Rail Fence Cipher with 2 rows so as to make it keyless')
-plaintext = input('Message text:\n')
+plaintext = input('Message text:')
 n = len(plaintext)
+encrypted = ''
+for i in range(n):
+	if n%2 == 0:
+		if i<n//2:
+			encrypted += plaintext[2*i]
+		else:
+			encrypted += plaintext[2*(i%(n//2))+1]
+	else:
+		if i<(n//2)+1:
+			encrypted += plaintext[2*i]
+		else:
+			encrypted += plaintext[2*((i-1)%(n//2))+1]
+print('Encrypted message:', encrypted)
+'''matrix implementation for encryption
 enc_mat = ['0']*n
 temp = ['0']*n
 enc_mat = [enc_mat, temp]
@@ -14,11 +28,10 @@ for i in range(n):
 	if enc_mat[1][i] != '0':
 		encrypted += enc_mat[1][i]
 print('Encrypted message', encrypted)
+'''
+
 #Now to decrypt
 n = len(encrypted)
-dec_mat = ['0']*n
-temp = ['0']*n
-dec_mat = [dec_mat, temp]
 decrypted = ''
 pair = 0
 for i in range(n):
